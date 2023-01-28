bbfdc3290182d88bf3447f36cd195b5fe8a04f4a
LouisASC22 <109673547+LouisASC22@users.noreply.github.com>
2023-01-27 22:16:45 -0500

    Add files via upload

---
+++ LouisASC22.github.io/script.js
@@ -0,0 +1,7 @@
+// not much here yet
+
+// sends a message to the console when the page loads
+window.addEventListener("load", () => {
+    console.log("This function is executed once the page is fully loaded");
+});
+
