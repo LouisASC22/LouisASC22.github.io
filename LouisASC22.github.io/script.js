44aae69963bf8b1699217c5eee0039da8d1f4ec3
LouisASC22 <109673547+LouisASC22@users.noreply.github.com>
2023-01-27 22:18:18 -0500

    Delete LouisASC22.github.io directory

--- LouisASC22.github.io/script.js
+++
@@ -1,7 +0,0 @@
-// not much here yet
-
-// sends a message to the console when the page loads
-window.addEventListener("load", () => {
-    console.log("This function is executed once the page is fully loaded");
-});
-
